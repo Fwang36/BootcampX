bf491832e305c66e4725341e8ccd112e8cc5f2c1
Francis <fwang36@gmail.com>
2022-03-07 14:52:33 -0500

    added large cohorts query

---
+++ 3_queries_group_by/1_large_cohorts.sql
@@ -0,0 +1,5 @@
+SELECT cohorts.name, count(students.id) as student_count
+FROM cohorts JOIN students ON cohort_id = cohorts.id
+GROUP BY cohorts.name
+HAVING count(students.id) >= 18
+ORDER BY count(students.id)
